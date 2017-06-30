b0b9293c6441c9abdec02d329a352b7aab880f06
zeng <zeng198821@126.com>
2017-06-30 18:12:04 +0800

    测试ui-router

--- src/main/webapp/input/inputNumber/app.js
+++ src/main/webapp/input/inputNumber/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by zeng on 17-6-23.
  */
-var app = angular.module("myApp",['yInputModule']);
+var app = angular.module("myApp",['yInputNumModule']);
 app.controller("numCtrl", function ($scope) {
     $scope.age=2;
     $scope.cc = function () {
